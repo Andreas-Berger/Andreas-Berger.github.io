49d5558e5d40ef39faed0672eb22156891e001e8
Andreas-Berger <andreas-berger@github.com>
2020-07-23 19:28:05 +0200

    stage it please

---
+++ stage/precache-manifest.35a554d5f06aa3c187b24ad03a61d4a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5303d6ffe4f072a1887c605a26ee6027",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f14782990a57b8f36d2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "50d86ecf8d4cdf4811a4",
+    "url": "/static/js/2.8b16e6c5.chunk.js"
+  },
+  {
+    "revision": "12433edcb03df6ae3e146553430924e1",
+    "url": "/static/js/2.8b16e6c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f14782990a57b8f36d2",
+    "url": "/static/js/main.0fb861c1.chunk.js"
+  },
+  {
+    "revision": "21275f2a40ee4630f124",
+    "url": "/static/js/runtime-main.c3747b82.js"
+  }
+]);
